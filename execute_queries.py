d6cf7e711d80310da8eaab09910bce2cb6cfe1ba
Maxat Kulmanov <coolmaksat@gmail.com>
2024-08-28 15:30:42 +0900

    execute queries

--- execute_queries.py
+++ execute_queries.py
@@ -1,6 +1,7 @@
 import click as ck
 import requests
 import json
+from pathlib import Path
 
 @ck.command()
 @ck.option('--queries-file', '-qf', help='An input file with SPARQL queries')
@@ -9,7 +10,10 @@ def main(queries_file, output_folder):
     with open(queries_file) as f:
         queries = json.loads(f.read())
     for i, query in enumerate(queries):
-        print(f'Running query 1:', query['label'])
+        output_file = Path(output_folder) / f'{i:03d}.json'
+        if output_file.exists():
+            continue
+        print(f'Running query {i}:', query['label'])
         result = requests.get(
             query['sparql_endpoint'],
             params={'format': 'json', 'query': query['sparql_query']})
@@ -20,7 +24,7 @@ def main(queries_file, output_folder):
         else:
             query['error'] = f'Error with status code {result.status_code}'
     
-        with open(Path(output_folder) / f'{i:03d}.json', 'w') as f:
+        with open(output_file, 'w') as f:
             f.write(json.dumps(query))
 
 if __name__ == '__main__':
